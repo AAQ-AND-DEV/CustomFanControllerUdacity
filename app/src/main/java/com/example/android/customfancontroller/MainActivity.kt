b918a768edfb2b36a8583df52627d82825c27977
aaq-and-dev <aaronquaday@gmail.com>
2020-12-31 15:59:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android.customfancontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.size
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val cl = findViewById<ConstraintLayout>(R.id.cl)
+
+    }
+}
